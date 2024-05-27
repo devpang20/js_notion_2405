7454cee837802e0f4ed6cf5420da19b9415e8fe6
sik2 <bickwang123@gmail.com>
2024-05-27 16:16:36 +0900

    사이드바 List, Page 컴포넌트 분리

---
+++ 4기 홍길동/src/components/sidebar/PostList.js
@@ -0,0 +1,42 @@
+export default function PostList({ $target, initalState }) {
+    const $postList = document.createElement('div')
+    $postList.className = 'postList'
+    $target.appendChild($postList)
+
+    this.createTreeView = (data) => {
+        console.log('createTreeView', data)
+        let str = ''
+
+        for (const key in data) {
+            if (data[key].documents.length > 0) {
+                str += `<li class="dataList">📄 ${data[key].title}
+                  <button class="addBtn">➕</button>
+                  <button class="delBtn">🗑️</button>
+                    <ul>${this.createTreeView(data[key].documents)}</ul>
+                  </li>`
+            } else {
+                str += `<li class="dataList">📄 ${data[key].title}
+                  <button class="addBtn">➕</button>
+                  <button class="delBtn">🗑️</button>
+                </li>`
+            }
+        }
+
+        return str
+    }
+
+    this.state = initalState
+    $postList.innerHTML = `<ul>
+      ${this.state
+          .map(
+              (document) => `<li class="dataList">📄 ${document.title}
+                <butto class="addBtn">➕</button>
+                <button class="delBtn">🗑️</button>
+              </li>
+          ${document.documents.length > 0 ? `<ul>${this.createTreeView(document.documents)}</ul>` : ''}
+          `,
+          )
+          .join('')}
+    </ul>
+  `
+}
--- 4기 홍길동/src/components/sidebar/PostPage.js
+++ 4기 홍길동/src/components/sidebar/PostPage.js
@@ -1,67 +1,9 @@
+import PostList from './PostList.js'
+
 export default function PostPage({ $target, initalState }) {
     const $page = document.createElement('div')
-    $page.className = 'postList'
+    $page.className = ''
     $target.appendChild($page)
 
-    //   [
-    //     {
-    //         id: 1, // Document id
-    //         title: '노션을 만들자', // Document title
-    //         documents: [
-    //             {
-    //                 id: 2,
-    //                 title: '블라블라',
-    //                 documents: [
-    //                     {
-    //                         id: 3,
-    //                         title: '함냐함냐',
-    //                         documents: [],
-    //                     },
-    //                 ],
-    //             },
-    //         ],
-    //     },
-    //     {
-    //         id: 4,
-    //         title: 'hello!',
-    //         documents: [],
-    //     },
-    // ]
-
-    this.createTreeView = (data) => {
-        console.log('createTreeView', data)
-        let str = ''
-
-        for (const key in data) {
-            if (data[key].documents.length > 0) {
-                str += `<li class="dataList">📄 ${data[key].title}
-                  <button class="addBtn">➕</button>
-                  <button class="delBtn">🗑️</button>
-                    <ul>${this.createTreeView(data[key].documents)}</ul>
-                  </li>`
-            } else {
-                str += `<li class="dataList">📄 ${data[key].title}
-                  <button class="addBtn">➕</button>
-                  <button class="delBtn">🗑️</button>
-                </li>`
-            }
-        }
-
-        return str
-    }
-
-    this.state = initalState
-    $page.innerHTML = `<ul>
-      ${this.state
-          .map(
-              (document) => `<li class="dataList">📄 ${document.title}
-                <butto class="addBtn">➕</button>
-                <button class="delBtn">🗑️</button>
-              </li>
-          ${document.documents.length > 0 ? `<ul>${this.createTreeView(document.documents)}</ul>` : ''}
-          `,
-          )
-          .join('')}
-    </ul>
-  `
+    new PostList({ $target: $page, initalState })
 }
