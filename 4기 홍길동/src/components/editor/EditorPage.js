1f086f9f19537f6a5f9339e05b113d389023b870
sik2 <bickwang123@gmail.com>
2024-05-27 16:31:39 +0900

    에디터 페이지 구현

--- 4기 홍길동/src/components/editor/EditorPage.js
+++ 4기 홍길동/src/components/editor/EditorPage.js
@@ -1,7 +1,11 @@
 export default function EditorPage({ $target }) {
     const $page = document.createElement('div')
 
-    $page.innerHTML = 'editor'
+    $page.innerHTML = `
+    <div class="editorDiv">
+      <input type="text" class="editorTitle" placeholder="제목없음" />
+      <textarea class="editorContent" placeholder="내용을 입력하세요"></textarea>
+    </div>`
 
     $target.appendChild($page)
 }
